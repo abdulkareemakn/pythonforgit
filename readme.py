a15037b7db420cfbf49e6885a093759bba553df5
abdulkareemakn <abdulkareemakn@proton.me>
2024-09-22 15:06:50 +0500

    Read this and follow the instructions

---
+++ readme.py
@@ -0,0 +1,6 @@
+print("Fork this repository")
+print("Then clone the repository locally")
+print("change the hello.py file")
+print("also create a new file")
+print("Commit both of these")
+print("Make separate pull requests")
