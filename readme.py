d9b9639e74ef19e9093f06d79672c771d1873b3c
hussaindurrani <hussaindurrani85@gmail.com>
2024-11-08 19:32:33 +0500

    deleted instructions

--- readme.py
+++
@@ -1,6 +0,0 @@
-print("Fork this repository")
-print("Then clone the repository locally")
-print("change the hello.py file")
-print("also create a new file")
-print("Commit both of these")
-print("Make separate pull requests")
